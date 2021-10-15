741e1573a03ebeacd5828b64a3922e1eaca203be
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-10-15 22:42:05 +0700

    Lab7 complet

---
+++ Lab07_Creational_Pattern/SoftArchDesign-compat/SoftArchDesign/src/main/java/creational/BookMetadataExporter.java
@@ -0,0 +1,12 @@
+package creational;
+
+import java.io.PrintStream;
+
+public abstract class BookMetadataExporter extends BookCollection {
+
+    public void export(PrintStream stream) {
+
+        stream.println(this.formatString());
+    }
+    public abstract String formatString();
+}
